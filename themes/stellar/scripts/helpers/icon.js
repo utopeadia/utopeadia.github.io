4b53edebcaff272e1fc1350fa3c77ba3d990e5fa
HowieWu <98788152+how1ewu@users.noreply.github.com>
2024-02-02 23:01:49 +0800

    update

--- themes/stellar/scripts/helpers/icon.js
+++ themes/stellar/scripts/helpers/icon.js
@@ -1,16 +1,5 @@
 'use strict';
 
-hexo.extend.helper.register('icon', function(key) {
-  const { icons } = hexo.theme.config
-  var result = ''
-  if (icons[key]) {
-    result = icons[key]
-  } else {
-    result = key
-  }
-  if (result.startsWith('/') || result.startsWith('https://') || result.startsWith('http://')) {
-    return `<img src="${result}" />`
-  } else {
-    return result
-  }
+hexo.extend.helper.register('icon', function(key, args) {
+  return hexo.utils.icon(key, args)
 })
