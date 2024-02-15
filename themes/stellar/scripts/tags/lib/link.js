a1c877bd274a9c61b0ad96702ad0ab16360e4917
howiewu <9246583-howiewu@users.noreply.gitlab.com>
2024-02-15 09:21:01 +0800

    update

--- themes/stellar/scripts/tags/lib/link.js
+++ themes/stellar/scripts/tags/lib/link.js
@@ -10,6 +10,14 @@
 module.exports = ctx => function(args) {
   const full_url_for = require('hexo-util').full_url_for.bind(ctx)
   args = ctx.args.map(args, ['icon', 'desc'], ['url', 'title'])
+  if (args.url == null) {
+    return '';
+  }
+  const url = full_url_for(args.url)
+  args.api = ctx.theme.config.tag_plugins.linkcard.api
+  if (args.api) {
+    args.api = args.api.replace('${href}', url)
+  }
   var autofill = []
   if (!args.title) {
     autofill.push('title')
@@ -27,6 +35,9 @@ module.exports = ctx => function(args) {
     el += ' target="_blank" rel="external nofollow noopener noreferrer"'
   }
   el += ' cardlink'
+  if (args.api) {
+    el += ` api="${args.api}"`
+  }
   el += ' autofill="'
   el += autofill.join(',')
   el += '"'
